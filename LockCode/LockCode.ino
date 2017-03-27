459459ce59bce9f23d9715c07c0ca7a1b66dcdc6
Harsh Dawda <Harsh Dawda>
2017-03-27 10:05:39 -0400

    work with this code, i still hv no idea whts wrong

--- LockCode/LockCode.ino
+++ LockCode/LockCode.ino
@@ -1,16 +1,17 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
-#include <Servo.h> 
+#include <Servo.h>
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int servoPin = 9;
-Servo servo;  
+Servo servo;
 int angle = 0;
 //check pin before starting
 int RECV_PIN = 10;
-int lockcode[] = {1,2,3,4};
-int input[4];
+int lockcode[] = {16738455, 16738455, 16738455, 16738455};
+int input;
+int possibleinput[10] = {16738455, 16724175, 16718055, 16743045, 16716015, 16726215, 16734885, 16728765, 16730805, 16732845};
 int codelimit = 0;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -20,43 +21,53 @@ void setup()
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
   servo.attach(servoPin);
-   // set up the LCD's number of columns and rows:
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("Locked");
 }
 
 void loop() {
-if (codelimit != 4){
- if (irrecv.decode(&results))
-    {
-     Serial.println(results.value, DEC);
-     lcd.clear();
-     lcd.print(results.value, DEC);
-     input[codelimit] = (results.value, DEC);
-     irrecv.resume();
+  if (codelimit == 0) {
+    if (irrecv.decode(&results)) {
+      for (int i = 0; i < 10; i++) {
+        if ((results.value, DEC) == possibleinput[i]) {
+          break;
+        } else {
+          continue;
+        }
+
+      }
+      {
+        Serial.println(results.value, DEC);
+        lcd.clear();
+        lcd.print(results.value, DEC);
+        input = results.value, DEC;
+        irrecv.resume();
+      }
+      codelimit++;
+      delay(100);
     }
-    codelimit++;
-    delay(2000);
-    }
-   else{
-    if (input == lockcode){
-      for(angle = 0; angle < 180; angle++)  
-  {                                  
-    servo.write(angle);               
-    delay(15);                   
-  } 
-      lcd.clear();
+  }
+  else if (input == 16738455) {
+      lcd.setCursor(0, 1);
       lcd.print("Unlocked");
+      Serial.println("Unlocked");
+      for (angle = 0; angle < 180; angle++)
+      {
+        servo.write(angle);
+        delay(15);
+      }
     }
-    else {
-      lcd.setCursor(0,1);
-      lcd.print("Wrong passcode");
-    }
-    codelimit -= 4;
-    for( int i = 0; i < sizeof(input);i++)
-   input[i] = (char)0;
-   }
-}
+    //    else {
+    //      lcd.setCursor(0,1);
+    //      lcd.print("Wrong passcode");
+    //    }
+    codelimit -= 1;
+    ////    for( int i = 0; i < 1;i++){
+    ////   input[i] = (char)0;
+    //   }
+  }
+
 
 
