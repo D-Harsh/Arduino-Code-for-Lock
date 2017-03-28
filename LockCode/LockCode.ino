bfa05b73964c7f7f87f574ded6429ee1a47cfc1c
Harsh Dawda <Harsh Dawda>
2017-03-28 17:24:25 -0400

    I need to test this

--- LockCode/LockCode.ino
+++ LockCode/LockCode.ino
@@ -1,73 +1,61 @@
-#include <IRremote.h>
-#include <IRremoteInt.h>
-#include <Servo.h>
 #include <LiquidCrystal.h>
-
+const int SW_pin = 2;
+const int X_pin = 0;
+const int Y_pin = 1;
+int counter = 0;
+int limit = 0;
+char *input[4]; 
+char *passcode[4] = {"Right","Right","Right","Right"};
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int servoPin = 9;
-Servo servo;
-int angle = 0;
-//check pin before starting
-int RECV_PIN = 10;
-int lockcode[] = {16738455, 16738455, 16738455, 16738455};
-int input;
-int possibleinput[10] = {16738455, 16724175, 16718055, 16743045, 16716015, 16726215, 16734885, 16728765, 16730805, 16732845};
-int codelimit = 0;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
 
-void setup()
-{
-  Serial.begin(9600);
-  irrecv.enableIRIn(); // Start the receiver
-  servo.attach(servoPin);
-  // set up the LCD's number of columns and rows:
+void setup() {
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd.print("Locked");
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (codelimit == 0) {
-    if (irrecv.decode(&results)) {
-      for (int i = 0; i < 10; i++) {
-        if ((results.value, DEC) == possibleinput[i]) {
-          break;
-        } else {
-          continue;
-        }
-
-      }
-      {
-        Serial.println(results.value, DEC);
-        lcd.clear();
-        lcd.print(results.value, DEC);
-        input = results.value, DEC;
-        irrecv.resume();
-      }
-      codelimit++;
-      delay(100);
+  if (counter < 4) {
+    if (analogRead(X_pin) > 1000) {
+      lcd.setCursor(0,1);
+      lcd.print("Down");
+      *input[counter] = "Down";
+      counter++;
     }
-  }
-  else if (input == 16738455) {
-      lcd.setCursor(0, 1);
-      lcd.print("Unlocked");
-      Serial.println("Unlocked");
-      for (angle = 0; angle < 180; angle++)
-      {
-        servo.write(angle);
-        delay(15);
-      }
+    if (analogRead(X_pin) < 20) {
+      lcd.setCursor(0,1);
+      lcd.print("Up");
+      *input[counter] = "Up";
+      counter++;
     }
-    //    else {
-    //      lcd.setCursor(0,1);
-    //      lcd.print("Wrong passcode");
-    //    }
-    codelimit -= 1;
-    ////    for( int i = 0; i < 1;i++){
-    ////   input[i] = (char)0;
-    //   }
+    if (analogRead(Y_pin) > 1000) {
+      lcd.setCursor(0,1);
+      lcd.print("Left");
+      *input[counter] = "Left";
+      counter++;
+    }
+    if (analogRead(Y_pin) < 20) {
+      lcd.setCursor(0,1);
+      lcd.print("Right");
+      *input[counter] = "Right";
+      counter++;
+    }
+    delay(500);
   }
+  else {
+      if ((*input[0]+*input[1]+*input[2]+*input[3]) == (*passcode[0]+*passcode[1]+*passcode[2]+*passcode[3])){
+          lcd.clear();
+          lcd.print("Accessing Matrix");
+      else if (limit < 3){
+        lcd.setCursor(0,1);
+        limit++;
+        lcd.print("Wrong Attempt #"+limit);
+      }
+  }
+}
+}
 
 
 
